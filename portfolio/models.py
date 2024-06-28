0705198e82e98e0710184bcb084ebffd09dbe7d2
githubdagi taxallusingiz <norovnorbek07@gmail.com>
2024-06-28 12:20:22 +0500

    first commit

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=200)
+    content = models.TextField()
+    def __str__(self):
+        return f"{self.name} by {self.email}"
+ 
+    
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+from .models import Contact
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
 
 def home_view(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
 
 def contact_view(request):
-    return render(request,'contact.html')
+    if request.method == 'POST':
+        try:
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            content = request.POST.get('content')
+            new_content = Contact(name=name, email=email, content=content)
+            new_content.save()
+            messages.success(request,"your messages succesfully sent!")
+            return HttpResponseRedirect(reverse('home-page'))
+
+            return render(request, 'contact.html', {'success': True})
+        except:
+            # Feedback message for error
+            return render(request, 'contact.html', {'error': True})
+
+    return render(request, 'contact.html')
